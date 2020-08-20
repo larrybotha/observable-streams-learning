a9f4f565958e794ee57952589f38d8fb221bc794
Larry Botha <larry@fixate.it>
2020-08-19 21:32:41 -0500

    begin beta animator

---
+++ standalone-projects/svelte-animated-paths/src/svg-animator-beta/enums.js
@@ -0,0 +1,13 @@
+const AnimationState = {
+  atRest: 'at-rest',
+  animating: 'animating',
+  complete: 'complete',
+};
+
+const PathState = {
+  atRest: 'at-rest',
+  expanding: 'expanding',
+  collapsing: 'collapsing',
+};
+
+export {AnimationState, PathState};
