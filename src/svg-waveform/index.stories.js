f0589935128dc9210f14c567aee985a0eab3781b
Larry Botha <larry@fixate.it>
2020-08-21 22:19:24 -0500

    remove project

--- src/svg-waveform/index.stories.js
+++
@@ -1,7 +0,0 @@
-import Index from './index.svelte';
-
-export default {title: 'SVG Waveform'};
-
-export const index = () => ({
-  Component: Index,
-});
--- src/svg-waveform/utils.ts
+++
@@ -1,6 +0,0 @@
-function logisticalMap(r: number, x: number) {
-  // f(xt+1) = R * xt * (1 - xt)
-  return r * x * (1 - x);
-}
-
-export {logisticalMap};
